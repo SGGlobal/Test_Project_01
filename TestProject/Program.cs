2eb5eaf92e7ec3e5ff3b1a2704e48bb3511ddbf0
SGGlobal <100097110+SGGlobal@users.noreply.github.com>
2023-10-13 14:35:39 -0500

    Add files via upload

---
+++ TestProject/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hell0, W0rld!");
+
+// Random & Console for = System.R#%!
+Random dice = new Random();
+int roll = dice.Next(1, 9);
+Console.WriteLine(dice.Next(1,9));
